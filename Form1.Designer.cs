08fa79ad5abde1728c83d23615dab50cc4fefac5
eliasholm8 <eliasholm8@gmail.com>
2022-03-06 23:25:35 +0100

    Added snake position check, corrected offset, started implementation of snake collision check.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,38 +31,53 @@
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.lblDebug = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblStartGame = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.panel1.Location = new System.Drawing.Point(39, 30);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(583, 337);
+            this.panel1.Size = new System.Drawing.Size(600, 360);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // timer1
             // 
-            this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // lblDebug
+            // lblScore
             // 
-            this.lblDebug.AutoSize = true;
-            this.lblDebug.Location = new System.Drawing.Point(73, 394);
-            this.lblDebug.Name = "lblDebug";
-            this.lblDebug.Size = new System.Drawing.Size(55, 15);
-            this.lblDebug.TabIndex = 1;
-            this.lblDebug.Text = "lblDebug";
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Pricedown Bl", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblScore.Location = new System.Drawing.Point(671, 134);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(83, 24);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "lblScore";
+            // 
+            // lblStartGame
+            // 
+            this.lblStartGame.AutoSize = true;
+            this.lblStartGame.BackColor = System.Drawing.Color.LimeGreen;
+            this.lblStartGame.Font = new System.Drawing.Font("Pricedown Bl", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblStartGame.Location = new System.Drawing.Point(671, 63);
+            this.lblStartGame.Name = "lblStartGame";
+            this.lblStartGame.Size = new System.Drawing.Size(160, 24);
+            this.lblStartGame.TabIndex = 2;
+            this.lblStartGame.Text = "Starta nytt spel";
+            this.lblStartGame.Click += new System.EventHandler(this.lblStartGame_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblDebug);
+            this.ClientSize = new System.Drawing.Size(857, 450);
+            this.Controls.Add(this.lblStartGame);
+            this.Controls.Add(this.lblScore);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -77,6 +92,7 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label lblDebug;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblStartGame;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,7 @@ namespace Snake.NET
         public int direction = 0;
         public int stepLength = 20;
         public int dx = 0;
-        public int dy = 5;
+        public int dy = 0;
         public int size = 20;
 
         Random rnd = new Random();
@@ -38,16 +38,50 @@ namespace Snake.NET
             }
 
         }
-        public void NewFood()
+
+        private bool CheckIfSnake(Rectangle rec)
         {
-
-            food = new Rectangle(rnd.Next(0, panel1.Width),rnd.Next(0 ,panel1.Height),size,size);
+            bool IsOccupied = false;
+            foreach (var item in snakeList)
+            {
+                if (item.IntersectsWith(rec))
+                {
+                    IsOccupied = true;
+                }
+            }
+            return IsOccupied;
         }
+        /// <summary>
+        /// Placerar mat till snake. Placerar med samma mellanrum som ormens bredd.
+        /// </summary>
+        private void NewFood()
+        {
+            int x, y;
+            Rectangle temp;
 
+            x = rnd.Next(0, (panel1.Width / size)) * size;
+            y = rnd.Next(0, (panel1.Height / size)) * size;
+
+            temp = new Rectangle(x, y, size, size);
+
+            if (!CheckIfSnake(temp))
+            {
+                food = temp;
+            }
+            else
+            {
+                NewFood();
+            }
+        }
+        private void StartGame()
+        {
+            snakeList.Add(new Rectangle(20, 20, size, size));
+            timer1.Start();
+            NewFood();
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
-            snakeList.Add(new Rectangle(40, 40, size, size));
-            NewFood();
+
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -83,14 +117,35 @@ namespace Snake.NET
             panel1.Invalidate();
             
             snakeList.Insert(0, new Rectangle(snakeList[0].X + dx, snakeList[0].Y + dy, size, size));
-            snakeList.RemoveAt(snakeList.Count - 1);
+
+            if (CheckIfSnake(snakeList[0]))
+            {
+                GameOver();
+            }
+            else
+            {
+                snakeList.RemoveAt(snakeList.Count - 1);
+                
+            }
+
 
             if (snakeList[0].IntersectsWith(food))
             {
                 snakeList.Insert(snakeList.Count, new Rectangle(snakeList[snakeList.Count - 1].X, snakeList[snakeList.Count - 1].Y, size, size));
                 NewFood();
             }
-            lblDebug.Text = (snakeList.Count() - 1).ToString();
+            lblScore.Text = $"Score: {(snakeList.Count() - 1)}";
         }
+
+        private void lblStartGame_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+        private void GameOver()
+        {
+            timer1.Stop();
+
+        }
+
     }
 }
