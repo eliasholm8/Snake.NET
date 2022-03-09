227889052be6d443f5613d558feb66badaff3123
eliasholm8 <eliasholm8@gmail.com>
2022-03-10 00:13:43 +0100

    Working game, all components work.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,11 +30,18 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblPauseScreen = new System.Windows.Forms.Label();
             this.lblGameOver = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.lblScore = new System.Windows.Forms.Label();
             this.lblStartGame = new System.Windows.Forms.Label();
             this.lblPause = new System.Windows.Forms.Label();
+            this.blinkTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timerTime = new System.Windows.Forms.Timer(this.components);
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lblHighScore = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,6 +49,7 @@
             // 
             this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.lblPauseScreen);
             this.panel1.Controls.Add(this.lblGameOver);
             this.panel1.Location = new System.Drawing.Point(39, 30);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
@@ -49,6 +57,17 @@
             this.panel1.Size = new System.Drawing.Size(600, 360);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblPauseScreen
+            // 
+            this.lblPauseScreen.AutoSize = true;
+            this.lblPauseScreen.Font = new System.Drawing.Font("Pricedown Bl", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblPauseScreen.Location = new System.Drawing.Point(195, 163);
+            this.lblPauseScreen.Name = "lblPauseScreen";
+            this.lblPauseScreen.Size = new System.Drawing.Size(217, 32);
+            this.lblPauseScreen.TabIndex = 5;
+            this.lblPauseScreen.Text = "Spelet är pausat";
+            this.lblPauseScreen.Visible = false;
             // 
             // lblGameOver
             // 
@@ -78,7 +97,7 @@
             this.lblStartGame.AutoSize = true;
             this.lblStartGame.BackColor = System.Drawing.Color.LimeGreen;
             this.lblStartGame.Font = new System.Drawing.Font("Pricedown Bl", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblStartGame.Location = new System.Drawing.Point(671, 63);
+            this.lblStartGame.Location = new System.Drawing.Point(673, 60);
             this.lblStartGame.Name = "lblStartGame";
             this.lblStartGame.Size = new System.Drawing.Size(160, 24);
             this.lblStartGame.TabIndex = 2;
@@ -97,10 +116,65 @@
             this.lblPause.Text = "Pausa";
             this.lblPause.Click += new System.EventHandler(this.lblPause_Click);
             // 
+            // blinkTimer
+            // 
+            this.blinkTimer.Interval = 50;
+            this.blinkTimer.Tick += new System.EventHandler(this.blinkTimer_Tick);
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Font = new System.Drawing.Font("Pricedown Bl", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblLevel.Location = new System.Drawing.Point(673, 259);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(70, 24);
+            this.lblLevel.TabIndex = 4;
+            this.lblLevel.Text = "Level: 1";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Pricedown Bl", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTime.Location = new System.Drawing.Point(670, 311);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 24);
+            this.lblTime.TabIndex = 5;
+            // 
+            // timerTime
+            // 
+            this.timerTime.Interval = 1000;
+            this.timerTime.Tick += new System.EventHandler(this.timerTime_Tick);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblInstructions.Font = new System.Drawing.Font("Pricedown Bl", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblInstructions.Location = new System.Drawing.Point(670, 416);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(145, 25);
+            this.lblInstructions.TabIndex = 6;
+            this.lblInstructions.Text = "instruktioner";
+            this.lblInstructions.Click += new System.EventHandler(this.lblInstructions_Click);
+            // 
+            // lblHighScore
+            // 
+            this.lblHighScore.AutoSize = true;
+            this.lblHighScore.Font = new System.Drawing.Font("Pricedown Bl", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblHighScore.Location = new System.Drawing.Point(670, 311);
+            this.lblHighScore.Name = "lblHighScore";
+            this.lblHighScore.Size = new System.Drawing.Size(98, 24);
+            this.lblHighScore.TabIndex = 7;
+            this.lblHighScore.Text = "highscore";
+            // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.ClientSize = new System.Drawing.Size(857, 450);
+            this.Controls.Add(this.lblHighScore);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblLevel);
             this.Controls.Add(this.lblPause);
             this.Controls.Add(this.lblStartGame);
             this.Controls.Add(this.lblScore);
@@ -124,5 +198,12 @@
         private System.Windows.Forms.Label lblStartGame;
         private System.Windows.Forms.Label lblGameOver;
         private System.Windows.Forms.Label lblPause;
+        private System.Windows.Forms.Timer blinkTimer;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label lblPauseScreen;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timerTime;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label lblHighScore;
     }
 }
---
+++ Form2.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Snake.NET
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnToGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Pricedown Bl", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(294, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Instruktioner";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(274, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Starta spelet genom att trycka på \"Starta nytt spel\".";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(141, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(441, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Styr ormen med piltangenterna och få poäng genom att träffa de röda matbitarna.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(141, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Åker du in i dig själv förlorar du. ";
+            // 
+            // btnToGame
+            // 
+            this.btnToGame.Location = new System.Drawing.Point(307, 356);
+            this.btnToGame.Name = "btnToGame";
+            this.btnToGame.Size = new System.Drawing.Size(185, 42);
+            this.btnToGame.TabIndex = 4;
+            this.btnToGame.Text = "Till spelet";
+            this.btnToGame.UseVisualStyleBackColor = true;
+            this.btnToGame.Click += new System.EventHandler(this.btnToGame_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnToGame);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnToGame;
+    }
+}
