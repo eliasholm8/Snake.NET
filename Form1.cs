89556acc33d4f0bb98376f4fe18922d41d87b6da
eliasholm8 <eliasholm8@gmail.com>
2022-03-10 00:13:43 +0100

    Working game, all components work.

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+ * Elias Holm 19NAA1
+ * https://github.com/eliasholm8/Snake.NET
+ * 
+ * Tillagda delar:
+ * Inforuta
+ * Starta nytt spel utan omstart
+ * Timer
+ * Levlar med varierande hastighet
+ * Start och paus knappar
+ * Blinkade GameOver
+ * Flera matbitar
+ * Ormen kan gå genom väggar
+ * Flerfärgad orm
+ * Grand Theft Auto tema på texten.
+ * Highscore
+ * Paus på Esc-knappen
+ * 
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,41 +28,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Snake.NET
 {
     public partial class Form1 : Form
     {
-        public int direction = 0;
+        public int highscore = 0;
+        public int direction = 2;
         public int stepLength = 20;
         public int dx = 20;
         public int dy = 0;
         public int size = 20;
+        public int level = 1;
+        public int score = 0;
+        public int time = 0;
+        public bool food1Eaten = true;
+        public bool food2Eaten = true;
         public bool isPaused = false;
 
         Random rnd = new Random();
 
-        public Rectangle food;
+        /// <summary>
+        /// Intierar matens platser.
+        /// </summary>
+        public Rectangle food1, food2;
+
+        /// <summary>
+        /// Initierar listan som sparar ormens delar.
+        /// </summary>
         List<Rectangle> snakeList = new List<Rectangle>();
+
         public Form1()
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// Startar spelet och återställer alla relevanta variabler.
+        /// </summary>
         private void StartGame()
         {
             dy = 0;
             dx = stepLength;
             snakeList.Clear();
             lblGameOver.Visible = false;
-            timer1.Start();
-            snakeList.Add(new Rectangle(20, 20, size, size));
+            ResetLevel();
+            UnPause();
+            StopBlink();
+            snakeList.Add(new Rectangle(0, 0, size, size));
             NewFood();
         }
-
+        /// <summary>
+        /// Renderar spelplanen med data ur <typeparamref name="snakeList"/> med varannnan färg.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.FillRectangle(Brushes.Red, food);
+            g.FillRectangle(Brushes.Red, food1);
+            g.FillRectangle(Brushes.Red, food2);
+
+            // i refererar till vilken position snakeList item har.
 
             foreach (var (rec, i) in snakeList.Select((rec, i) => (rec,i)))
             {
@@ -56,7 +104,11 @@ namespace Snake.NET
             }
 
         }
-
+        /// <summary>
+        /// Kontrollerar om någon del av <typeparamref name="snakeList"/> befinner sig på positionen <typeparamref name="rec"/> uppehåller sig på.
+        /// </summary>
+        /// <param name="rec"></param>
+        /// <returns></returns>
         private bool CheckIfSnake(Rectangle rec)
         {
             bool IsOccupied = false;
@@ -70,6 +122,53 @@ namespace Snake.NET
             return IsOccupied;
         }
         /// <summary>
+        /// Återställer level och hastighet.
+        /// </summary>
+        private void ResetLevel()
+        {
+            score = 0;
+            level = 1;
+            timer1.Interval = 100;
+            time = 0;
+            food1Eaten = true;
+            food2Eaten = true;
+
+            CheckLevel();
+        }
+        /// <summary>
+        /// Kontrollerar vilken level spelaren befinner sig på och uppdaterar score.
+        /// </summary>
+        private void CheckLevel()
+        {
+            lblLevel.Text = $"Level: {level}";
+            switch (score)
+            {
+                case 5:
+                    level = 2;
+                    timer1.Interval = 120;
+                    break;
+
+                case 10:
+                    level = 3;
+                    timer1.Interval = 100;
+                    break;
+
+                case 15:
+                    level = 4;
+                    timer1.Interval = 75;
+                    break;
+
+                case 20:
+                    level = 5;
+                    timer1.Interval = 60;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
         /// Placerar mat till snake. Placerar med samma mellanrum som ormens bredd.
         /// </summary>
         private void NewFood()
@@ -77,64 +176,51 @@ namespace Snake.NET
             int x, y;
             Rectangle temp;
 
+            // Ser till att maten placeras på ett rutnät jämnt med ormen.
+
             x = rnd.Next(0, (panel1.Width / size)) * size;
             y = rnd.Next(0, (panel1.Height / size)) * size;
 
             temp = new Rectangle(x, y, size, size);
 
-            if (!CheckIfSnake(temp))
+            if (food1Eaten)
             {
-                food = temp;
+                // Kontrollerar att den nya maten inte kolliderar med ormen och den andra matbiten.
+
+                if (temp.IntersectsWith(food1) || temp.IntersectsWith(food2) || CheckIfSnake(temp))
+                {
+                    NewFood();
+                }
+                else
+                {
+                    food1 = temp;
+                    food1Eaten = false;
+                }
             }
-            else
+            if (food2Eaten)
             {
-                NewFood();
-            }
+                if (temp.IntersectsWith(food1) || temp.IntersectsWith(food2) || CheckIfSnake(temp))
+                {
+                    NewFood();
+                }
+                else
+                {
+                    food2 = temp;
+                    food2Eaten = false;
+                }
+            }            
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            ShowInstructions();
+        }        
 
-        }
-
-        private void CheckKey()
+        private void ShowInstructions()
         {
-            switch (direction)
-            {
-                case 1:
-                    if (direction != 3)
-                    {
-                        dy = -stepLength;
-                        dx = 0;
-                    }
-                    break;
+            Form Form2;
 
-                case 2:
-                    if (direction != 4)
-                    {                        
-                        dx = stepLength;
-                        dy = 0;
-                    }
-                    break;
-
-                case 3:
-                    if (direction != 1)
-                    {
-                        dy = stepLength;
-                        dx = 0;
-                    }
-                    break;
-
-                case 4:
-                    if (direction != 2)
-                    {
-                        dx = -stepLength;
-                        dy = 0;
-                    }
-                    break;
-
-                default:
-                    break;
-            }
+            Form2 = new Form2();
+            Form2.Show();
         }
         private async void Form1_KeyDown(object sender, KeyEventArgs e)
         {           
@@ -178,72 +264,88 @@ namespace Snake.NET
                     }
                     break;
 
+                case Keys.Escape:
+                    PauseGame();
+                    break;
+
                 default:
                     break;
             }
-            
-
-
-            if (false)
-            {
-                if (e.KeyCode == Keys.Up)
-                {
-                    direction = 1;
-                    
-                }
-                else if (e.KeyCode == Keys.Down)
-                {
-                    direction = 3;
-                    
-                }
-                else if (e.KeyCode == Keys.Left)
-                {
-                    direction = 3;
-                    dx = -stepLength;
-                    dy = 0;
-                }
-                else if (e.KeyCode == Keys.Right)
-                {
-                    direction = 2;
-                    dx = stepLength;
-                    dy = 0;
-                }                   
-            }
-
         }
+        /// <summary>
+        /// Kontrollerar om ormens huvudbefinner sig på en matbit.
+        /// </summary>
+        private void CheckFood()
+        {
+            if (snakeList[0].IntersectsWith(food1))
+            {
+                food1Eaten = true;
+                EatFood();
+            }
+            if (snakeList[0].IntersectsWith(food2))
+            {
+                food2Eaten = true;
+                EatFood();
+            }
+            
+        }
+        /// <summary>
+        /// Förlänger ormen när medoden kallas. Används när ormen äter mat. 
+        /// </summary>
+        private void EatFood()
+        {
+            int xPosition, yPosition;
 
+
+            // Positionen där den nya delen av ormen ska sättas in i.
+
+            xPosition = snakeList[snakeList.Count - 1].X;
+            yPosition = snakeList[snakeList.Count - 1].Y;
+
+            snakeList.Insert(snakeList.Count, new Rectangle(xPosition, yPosition, size, size));
+            score++;           
+            CheckLevel();
+            NewFood();
+        }
+        /// <summary>
+        /// Flyttar ormen och kontollerar om den kolliderar med sig själv.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void timer1_Tick(object sender, EventArgs e)
         {
             Rectangle temp;
             int xHeadPosition,yHeadPosition;
 
+            // Nya positionen för huvudet
+
             xHeadPosition = snakeList[0].X + dx;
             yHeadPosition = snakeList[0].Y + dy;
 
-            int left, top;
-
-            left = panel1.Left;
-            top = panel1.Top;
-
+            // Skapar en temporär Rectanglevariabel.
 
             temp = new Rectangle(xHeadPosition, yHeadPosition, size, size);
 
-            if (xHeadPosition <= 0)
+            // Kontrollerar om ormen lämnnar kartan och flyttar den till andra sidan.
+
+            if (xHeadPosition < 0)
             {
-                temp = new Rectangle(panel1.Right - size, yHeadPosition, size, size);
+                temp = new Rectangle((panel1.Right + 1) - size, yHeadPosition, size, size);
             }
-            if (xHeadPosition >= panel1.Width)
+            if (xHeadPosition > panel1.Width)
             {
-                temp = new Rectangle(panel1.Left, yHeadPosition, size, size);
+                temp = new Rectangle(0, yHeadPosition, size, size);
             }
-            if (yHeadPosition <= 0)
+            if (yHeadPosition < 0)
             {
                 temp = new Rectangle(xHeadPosition, panel1.Height - size, size, size);
             }
-            if (yHeadPosition >= panel1.Height)
+            if (yHeadPosition > panel1.Height)
             {
-                temp = new Rectangle(xHeadPosition, 0 + size, size, size);
+                temp = new Rectangle(xHeadPosition, 0, size, size);
             }
+
+            // Kontrollerar om ormen kolliderar med sig själv.
 
             if (CheckIfSnake(temp))
             {
@@ -251,44 +353,103 @@ namespace Snake.NET
             }
             else
             {
+                // Flyttar ormen.
+
                 snakeList.Insert(0, temp);
                 
                 snakeList.RemoveAt(snakeList.Count - 1);           
             }
             panel1.Invalidate();
-
-            if (snakeList[0].IntersectsWith(food))
-            {
-                int xPosition, yPosition;
-
-                xPosition = snakeList[snakeList.Count - 1].X;
-                yPosition = snakeList[snakeList.Count - 1].Y;
-
-                snakeList.Insert(snakeList.Count, new Rectangle(xPosition, yPosition, size, size));
-                NewFood();
-            }
-            lblScore.Text = $"Score: {(snakeList.Count() - 1)}";
+            CheckFood();
+            
+            lblScore.Text = $"Score: {score}";
         }
-
+        /// <summary>
+        /// Startar spelet med StartGame metoden.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void lblStartGame_Click(object sender, EventArgs e)
         {
             StartGame();
+        }        
+        private void StopBlink()
+        {
+            blinkTimer.Stop();
+            lblGameOver.Visible = false;
         }
+
+        /// <summary>
+        /// Får texten att börja blinka när spelaren förlorat.
+        /// </summary>
+        private void BlinkGameOver()
+        {
+            blinkTimer.Enabled = true;
+        }
+        /// <summary>
+        /// Stoppar spelet vid förlust.
+        /// </summary>
         private void GameOver()
         {
             timer1.Stop();
-            lblGameOver.Visible = true;
+            BlinkGameOver();
             lblGameOver.Text = "wasted";
+            timerTime.Stop();
+            if (score > highscore)
+            {
+                highscore = score;
+            }
+            lblHighScore.Text = $"highscore: {highscore}";
         }
-        private void PauseGame()
+
+        /// <summary>
+        /// Avpausar spelet när ett nytt spel startas.
+        /// </summary>
+        private void UnPause()
         {
+            isPaused = false;
+            timer1.Enabled = true;
+            lblPauseScreen.Visible = false;
+            timerTime.Enabled = true;
+        }
+        /// <summary>
+        /// Pausar spelet.
+        /// </summary>
+        private void PauseGame()
+        {            
             isPaused = !isPaused;
-            timer1.Enabled = isPaused;
+            timer1.Enabled = !isPaused;
+            timerTime.Enabled = !isPaused;
+            lblPauseScreen.Visible = isPaused;
         }
 
         private void lblPause_Click(object sender, EventArgs e)
         {
             PauseGame();
         }
+        /// <summary>
+        /// Timern som gör det möjligt för gameover texten att blinka.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void blinkTimer_Tick(object sender, EventArgs e)
+        {            
+            lblGameOver.Visible = !lblGameOver.Visible;            
+        }
+        /// <summary>
+        /// Visar hur lång tid spelet har pågått.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timerTime_Tick(object sender, EventArgs e)
+        {
+            time++;
+            lblTime.Text = $"Tid: {time}";
+        }
+
+        private void lblInstructions_Click(object sender, EventArgs e)
+        {
+            ShowInstructions();
+        }                
     }
 }
